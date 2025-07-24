df65f1986c9e309441ccf0c7f0ea50bb2da63537
XiaoGeNintendo <34835642+XiaoGeNintendo@users.noreply.github.com>
2025-07-24 10:14:26 +0800

    rewrote globe using leaflet.js

--- globe.js
+++ globe.js
@@ -1,150 +1,67 @@
+let map;
+function loadMap(){
+    map = L.map('map').setView(myLoc, 4);
 
-function getMapFeatures(domestic=false) {
-    var seriesData = []
-    for (var i in data) {
-        var e = data[i]
-        if (e.globe == undefined || domestic && e.style==STYLE_ABROAD) {
-            continue
-        }
-        if (typeof e.globe[0] == 'number') {
-            seriesData.push({ name: e.content, geometry: { type: 'Point', coordinates: [e.globe[1], e.globe[0]] } })
-        } else {
-            for (var j in e.globe) {
-                var ee = e.globe[j]
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
 
-                seriesData.push({ name: e.content, geometry: { type: 'Point', coordinates: [ee[1], ee[0]] } })
-            }
-        }
-    }
-    return seriesData
-}
-function getLineFeatures(domestic=false) {
-    var seriesData = []
-    for (var i in data) {
-        var e = data[i]
-        if (e.globe == undefined || typeof e.globe[0] == "number" || domestic && e.style==STYLE_ABROAD) {
-            continue
-        }
-        for (var j = 0; j < e.globe.length - 1; j++) {
-            var e1 = e.globe[j]
-            var e2 = e.globe[j + 1]
-            seriesData.push({
-                name: e.content,
-                geometry: {
-                    type: 'LineString',
-                    coordinates: [
-                        [e1[1], e1[0]], // Yemen
-                        [e2[1], e2[0]] // Java
-                    ]
-                },
-                className: 'animated-line',
-                color: '#666'
-            })
-        }
-    }
+    //show my location with a marker
+    L.marker(myLoc).addTo(map)
+        .bindPopup('Home');
 
-    return seriesData
+    data.forEach(function(trip) {
+        drawTrip(trip);
+    });
 }
 
-(async () => {
-    const topology = await fetch(
-        'https://code.highcharts.com/mapdata/custom/world-highres.topo.json'
-    ).then(response => response.json());
-    
-    //Initialize cn map
-    $('#cn_map').highcharts('Map', {
-        title: {
-            text: 'Destinations in China',
-        },
-        subtitle: {
-            text: "Note that this map may contain error on borders. Please contact Jianshu Technology for the problem."
-        },
-        mapNavigation: {
-            enabled: true,
-            enableDoubleClickZoomTo: true,
-            buttonOptions: {
-                verticalAlign: 'bottom'
-            }
-        },
-        tooltip: {
-            headerFormat: '',
-            pointFormat: '<b>{point.name}</b>'
-        },
-        series: [{
-            mapData: Highcharts.maps['cn/china'],
-            name: 'Basemap',
-            borderColor: '#A0A0A0',
-            showInLegend: false
-        }, 
-        {
-            // Specify points using lat/lon
-            type: 'mappoint',
-            name: 'Tour Destinations',
-            color: '#0369f3',
-            marker: {
-                "fillColor": "white",
-                "lineColor": "black",
-                "lineWidth": 2,
-                "radius": 3
-            },
-            data: getMapFeatures(true)
-        }]
-    });
+function getBackgroundColor(style) {
+    //style is string of format "color: black; background-color: palegreen;"
+    var index = style.indexOf("background-color:");
+    if (index == -1) {  
+        return "#114514";
+    }
+    var start = index + "background-color:".length;
+    var end = style.indexOf(";", start);
+    return style.substring(start, end).trim().replace("palegreen","green");
+}
 
+const LARGE_RADIUS = 5;
+const SMALL_RADIUS = 3;
 
-    // Initialize the chart
-    var chart = Highcharts.mapChart('globe_map', {
-        chart: {
-            map: topology
-        },
+function drawTrip(trip){
+    if(typeof trip.globe[0] == 'number'){
+        //single trip
+        L.circleMarker(trip.globe, {
+            radius: LARGE_RADIUS,
+            weight: 1,
+            color: 'black',
+            fillColor: getBackgroundColor(trip.style),
+            fillOpacity: 0.5
+        }).addTo(map).bindPopup(trip.content);
+    }else{
+        //multiple locations
+        var latlngs = trip.globe
 
-        title: {
-            text: 'Destinations Around the World'
-        },
-        mapNavigation: {
-            enabled: true,
-            enableDoubleClickZoomTo: true,
-            buttonOptions: {
-                verticalAlign: 'bottom'
-            }
-        },
-        mapView: {
-            maxZoom: 30,
-            zoom: 3,
-            projection: {
-                name: 'Orthographic',
-                rotation: [-myLoc[1], -myLoc[0]],
-            },
-        },
-        tooltip: {
-            pointFormat: '{point.name}'
-        },
-        series: [
-            {
-                name: 'Journey Map',
-                showInLegend: false
-            },
-            {
-                type: 'mapline',
-                name: "Routes",
-                data: getLineFeatures()
-            },
-            {
-                name: "Destinations",
-                type: 'mappoint',
-                color: '#333',
-                marker: {
-                    "fillColor": "white",
-                    "lineColor": "black",
-                    "lineWidth": 2,
-                    "radius": 3
-                },
-                data: getMapFeatures(),
-            }
-        ]
-    });
+        L.polyline(latlngs, {
+            color: getBackgroundColor(trip.style),
+            weight: SMALL_RADIUS,
+            opacity: 0.5,
+            smoothFactor: 1
+        }).addTo(map).bindPopup(trip.content);
 
-    // chart.setView([myLoc[1],myLoc[0]],3);
-})();
-
-
+        //draw markers for each location
+        for(var i = 0; i < latlngs.length; i++) {
+            var latlng = latlngs[i];
+            var important = i == 0 || i == latlngs.length - 1;
+            L.circleMarker(latlng, {
+                radius: important ? LARGE_RADIUS : SMALL_RADIUS,
+                weight: important ? 1 : 0.5,
+                color: 'black',
+                fillColor: getBackgroundColor(trip.style),
+                fillOpacity: 0.5
+            }).addTo(map).bindPopup(trip.content + ' (' + (i + 1) + '/' + latlngs.length + ')');
+        }
+    }
+}
